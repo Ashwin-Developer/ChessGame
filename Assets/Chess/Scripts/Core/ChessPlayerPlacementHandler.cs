06254d14013e68c203bcf29a5984507f68ca79fc
Ashwin-Developer <88386942+Ashwin-Developer@users.noreply.github.com>
2024-07-31 14:06:47 +0530

    Possible moves completed

--- Assets/Chess/Scripts/Core/ChessPlayerPlacementHandler.cs
+++ Assets/Chess/Scripts/Core/ChessPlayerPlacementHandler.cs
@@ -1,48 +1,168 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Chess.Scripts.Core {
+    
+    public enum ChessPiece
+    {
+        pawn,
+        king,
+        rook,
+        Bhishop,
+        Queen,
+        Knight
+    }
 
     public class ChessPlayerPlacementHandler : MonoBehaviour {
 
         [SerializeField] public int row, column;
+        public ChessPiece piece;    
         private ChessBoardPlacementHandler _boardPlacementHandler;
-
-        [SerializeField] private LayerMask _pawn;
+        private int  _boardSize = 8;
 
         private void Start() {
             _boardPlacementHandler = ChessBoardPlacementHandler.Instance;
             transform.position = _boardPlacementHandler.GetTile(row, column).transform.position;
         }
 
-        private void Update()
+        internal void StraightMovements()
         {
-            if (Input.GetMouseButtonDown(0))
+            //for vertical highlights
+            for (int i = 0; i < _boardSize; i++)
             {
-                HandlePieceSelection();
+                if (i != row)
+                {
+                    _boardPlacementHandler.Highlight(i, column);
+                }
+            }
+
+            //for horizontal highlights
+            for (int i = 0; i < _boardSize; i++)
+            {
+                if (i != column)
+                {
+                    _boardPlacementHandler.Highlight(row, i);
+                }
             }
         }
 
-        private void HandlePieceSelection()
+        internal void DiagonalMovements()
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // For diagonal movements
+            int[] directions = { 1, -1 };
 
-            if (Physics.Raycast(ray, out hit, _pawn))
+            foreach (var dirX in directions)
             {
-                Debug.Log("Pawn is Clicked");
-                CalculatePossibleMoves();
+                foreach (var dirY in directions)
+                {
+                    int x = row;
+                    int y = column;
+                    while (true)
+                    {
+                        x += dirX;
+                        y += dirY;
+
+                        if (x >= 0 && x < _boardSize && y >= 0 && y < _boardSize)
+                        {
+                            // Highlight the tile
+                            _boardPlacementHandler.Highlight(x, y);
+
+                            // Stop if the tile is occupied (example: you can add additional logic for this)
+                            // Example: if (IsTileOccupied(x, y)) break;
+                        }
+                        else
+                        {
+                            // Break if out of board bounds
+                            break;
+                        }
+                    }
+                }
             }
         }
-
-        public void CalculatePossibleMoves()
+       
+        //Checking possible moves of the Pawn
+        internal void PawnPossibleMoves()
         {
-            Debug.Log("Possible moves has been called");
             _boardPlacementHandler.ClearHighlights();
-
-            for(int i = 2; i <= 3;  i++)
+            Debug.Log("Pawns possible moves called");
+            for (int i = 1; i <= 2; i++) 
             {
-                _boardPlacementHandler.Highlight(i, column);
+                _boardPlacementHandler.Highlight(row + i, column);
+            }
+        }
+
+        //Checking possible moves of the Rook
+        internal void RookPossibleMoves()
+        {
+            Debug.Log("Rook's possible moves called");
+            _boardPlacementHandler.ClearHighlights();
+            StraightMovements();
+        }
+
+        //Checking possible moves of the Bhishop
+        internal void BhishopPossibleMoves()
+        {
+            Debug.Log("Bishop's possible moves called");
+
+            _boardPlacementHandler.ClearHighlights();
+            DiagonalMovements();
+        }
+
+
+        //Checking possible moves of the Queen
+        internal void QueenPossibleMoves()
+        {
+            _boardPlacementHandler.ClearHighlights();
+            StraightMovements();
+            DiagonalMovements();
+        }
+
+        //Checking possible moves of the Knight
+        internal void KnightPossibleMoves()
+        {
+            Debug.Log("Knight's possible moves called");
+            _boardPlacementHandler.ClearHighlights();
+            int[,] knightMoves = new int[,]
+            {
+                { 2, 1 }, { 2, -1 }, { -2, 1 }, { -2, -1 },
+                { 1, 2 }, { 1, -2 }, { -1, 2 }, { -1, -2 }
+            };
+
+            for (int i = 0; i < knightMoves.GetLength(0); i++)
+            {
+                int newRow = row + knightMoves[i, 0];
+                int newCol = column + knightMoves[i, 1];
+
+                if (newRow >= 0 && newRow < _boardSize && newCol >= 0 && newCol < _boardSize)
+                {
+                    _boardPlacementHandler.Highlight(newRow, newCol);
+                }
+            }
+        }
+
+        //Checking possible moves of the King
+        internal void KingPossibleMovements()
+        {
+            Debug.Log("King's possible moves called");
+
+            // Possible move offsets for the king
+            _boardPlacementHandler.ClearHighlights();
+            int[,] kingMoves = new int[,]
+            {
+                { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 },
+                { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 }
+            };
+
+            for (int i = 0; i < kingMoves.GetLength(0); i++)
+            {
+                int newRow = row + kingMoves[i, 0];
+                int newCol = column + kingMoves[i, 1];
+
+                if (newRow >= 0 && newRow < _boardSize && newCol >= 0 && newCol < _boardSize)
+                {
+                    _boardPlacementHandler.Highlight(newRow, newCol);
+                }
             }
         }
 
--- Assets/Chess/Scripts/Core/GameManager.cs
+++ Assets/Chess/Scripts/Core/GameManager.cs
@@ -5,26 +5,60 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
-    public LayerMask pawn;
-
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) 
         {
-            HandlePieceSelection();
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log("RaySend");
+                var player = hit.collider.GetComponent<ChessPlayerPlacementHandler>();
+                Debug.Log("Player " + player);
+                PieceHandler(player);
+            }
         }
     }
 
-    private void HandlePieceSelection()
+    //Checking specific piece and calling the specific method
+    internal void PieceHandler(ChessPlayerPlacementHandler player)
     {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out hit))
+        
+        if(player != null)
         {
-            Debug.Log("Pawn is Clicked");
-            //ChessPlayerPlacementHandler.Instance.CalculatePossibleMoves();
+            if(player.piece == ChessPiece.pawn)
+            {
+                player.PawnPossibleMoves();
+            }
+
+            else if (player.piece == ChessPiece.rook)
+            {
+                player.RookPossibleMoves();
+            }
+
+            else if (player.piece == ChessPiece.Bhishop)
+            {
+                player.BhishopPossibleMoves();
+            }
+
+            else if(player.piece == ChessPiece.Queen)
+            {
+                player.QueenPossibleMoves();
+            }
+
+            else if(player.piece == ChessPiece.Knight)
+            {
+                player.KnightPossibleMoves();
+            }
+
+            else if (player.piece == ChessPiece.king)
+            {
+                player.KingPossibleMovements();
+            }
         }
     }
+
 }
+
+
